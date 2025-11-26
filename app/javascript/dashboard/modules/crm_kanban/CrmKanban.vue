a77513b325016e6a78206b2a5a608705a4de0306
natannunesm <natannunes.m@gmail.com>
2025-11-26 13:46:20 -0300

    Create CrmKanban.vue

---
+++ app/javascript/dashboard/modules/crm_kanban/CrmKanban.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="crm-kanban">
+    <h1>CRM Kanban</h1>
+    <p>Módulo customizado funcionando!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CrmKanban',
+};
+</script>
+
+<style scoped>
+.crm-kanban {
+  padding: 20px;
+}
+</style>
